5ce66d944316a1445846fa76586b2557d94313db
Breno <brenozende@gmail.com>
2021-03-04 15:25:38 -0300

    Corrigido: trocar chamada redundante do printf pelo print

--- src/main/java/ExercicioQuatro.java
+++ src/main/java/ExercicioQuatro.java
@@ -5,8 +5,8 @@ public class ExercicioQuatro {
         Scanner input = new Scanner(System.in);
         int valor = input.nextInt();
         for (int i = 0; i < valor; i++) {
-            System.out.printf("Incomoda ");
+            System.out.print("Incomoda ");
         }
-        System.out.printf("muito mais");
+        System.out.print("muito mais");
     }
 }
