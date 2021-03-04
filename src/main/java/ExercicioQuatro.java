8ae8a1c98a613dc3b4e2222408d9007ae9735137
Breno <brenozende@gmail.com>
2021-03-04 15:24:39 -0300

    Adicionado exercicio 4

---
+++ src/main/java/ExercicioQuatro.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ExercicioQuatro {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int valor = input.nextInt();
+        for (int i = 0; i < valor; i++) {
+            System.out.printf("Incomoda ");
+        }
+        System.out.printf("muito mais");
+    }
+}
